000c867765e0b75aaf58fd0551b683957c70d567
Nam <namdeveloper.ca@gmail.com>
2023-12-06 21:21:59 +0700

    Fix heading section

--- components/pages/about/HeadSection.tsx
+++ components/pages/about/HeadSection.tsx
@@ -1,7 +1,9 @@
 const HeadSection = () => {
   return (
     <div className='mb-16'>
-      <h2 className='font-heading font-medium text-[3rem] leading-[1]'>sozonome</h2>
+      <h2 className='font-heading font-medium text-[2.25rem] leading-[1.2] lg:text-[3rem] lg:leading-[1]'>
+        sozonome
+      </h2>
     </div>
   )
 }
