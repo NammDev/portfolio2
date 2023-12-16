428fff6461d7b46a57239b3dfcae1a35c90e39e6
Nam <namdeveloper.ca@gmail.com>
2023-12-16 22:01:40 +0700

    finish command

--- components/projects/other/index.tsx
+++ components/projects/other/index.tsx
@@ -50,7 +50,7 @@ const OtherProjects = () => {
     <>
       <Link
         href='/projects'
-        className='mb-[22px] bg-[#414042] h-8 text-sm min-w-[2rem] font-sans transition-all items-center inline-flex appearance-none justify-center select-none relative whitespace-nowrap outline-none outline-offset-2 leading-[1.2] font-semibold text-[rgba(255,255,255,0.92)] rounded-2xl border-[rgba(255,255,255,0.16)] ps-3 pe-3 border-0 hover:border-2 hover:border-gray-500 hover:shadow-sm hover:bg-[rgba(255,255,255,0.16)]'
+        className='mb-[22px] bg-cardBorder1 h-8 text-sm min-w-[2rem] font-sans transition-all items-center inline-flex appearance-none justify-center select-none relative whitespace-nowrap outline-none outline-offset-2 leading-[1.2] font-semibold text-buttonForeground1 rounded-2xl ps-3 pe-3 border-0 hover:border-2 hover:shadow-sm hover:bg-buttonBackground1'
         // onClick={handleBackToFeaturedProjects}
       >
         <span className='inline-flex items-center shrink-0 me-2'>
